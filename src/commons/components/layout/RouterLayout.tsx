7137075c30b5350aa5540f5d5aa80b9df6242c45
Cho JeongTaek <cjt3591@gmail.com>
2024-09-01 16:24:52 +0900

    📦️ Chore :  프로젝트 셋팅

---
+++ src/commons/components/layout/RouterLayout.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../../../pages/Home';
+
+export default function RouterLayout() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
