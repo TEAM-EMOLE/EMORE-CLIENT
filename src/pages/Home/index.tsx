a9694522ee72b78e09696e5d6f74a404b0025856
Moondy217 <moondaye0217@gmail.com>
2024-09-25 10:08:31 +0900

    💄 Design:  메인페이지 컨테이너 Width 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div className="flex justify-center items-center w-full h-full">
       {/* 홈페이지 컨테이너 */}
-      <div className="w-[375px] h-[635px] flex justify-center items-center border border-red-500">
+      <div className="w-[600px] h-full flex justify-center items-center border border-red-500">
         <div>
           메인페이지
         </div>
