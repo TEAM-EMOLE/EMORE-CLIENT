29a5914ebe97d34dba75f3ea49b2226de386aa10
Moondy217 <moondaye0217@gmail.com>
2024-10-02 10:40:05 +0900

    💄 Design:  Navbar 수직 정렬로 변경

--- src/commons/components/Navbar.tsx
+++ src/commons/components/Navbar.tsx
@@ -4,8 +4,8 @@ type NavbarProps = {
   
   export default function Navbar({ title }: NavbarProps) {
     return (
-      <div className="flex justify-center items-center w-full h-full">
-        <div className="h-[48px] flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <div className="w-[600px] h-[48px] flex justify-center items-center">
           {title}
         </div>
       </div>
