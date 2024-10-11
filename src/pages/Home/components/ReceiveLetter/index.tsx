c6cd2ca8c1c048945fd90e0b50e428579ff7bf6a
youngju6143 <youngju6143@naver.com>
2024-10-11 20:51:35 +0900

    💄 Design:  편지함 버튼에 cursor: pointer 적용

--- src/pages/Home/components/ReceiveLetter/index.tsx
+++ src/pages/Home/components/ReceiveLetter/index.tsx
@@ -5,7 +5,7 @@
 
 export default function ReceiveLetter() {
   return (
-    <div className="px-3 w-11/12 h-[54px] flex items-center justify-between border-2">
+    <div className="px-3 w-11/12 h-[54px] flex items-center justify-between border-2 cursor-pointer">
         <p>받은 편지함</p>
         <button>)</button>
     </div>
--- src/pages/Home/components/SendLetter/index.tsx
+++ src/pages/Home/components/SendLetter/index.tsx
@@ -4,7 +4,7 @@
 
 export default function SendLetter() {
   return (
-    <div className="flex items-center justify-between px-3 w-11/12 h-[54px] border-2">
+    <div className="flex items-center justify-between px-3 w-11/12 h-[54px] border-2 cursor-pointer">
         <p>내가 쓴 편지함</p>
         <button>)</button>
     </div>
