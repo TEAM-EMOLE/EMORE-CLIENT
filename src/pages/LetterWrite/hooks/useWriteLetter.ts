1755cd4addf84f83522d3a7933de5eebc6e85bb8
이원찬 <twoone14@tukorea.ac.kr>
2024-10-24 21:25:35 +0900

    ✨ Feat :  쓰로틀링으로 토스트 메시지 로직 단순화

--- src/pages/LetterWrite/hooks/useWriteLetter.ts
+++ src/pages/LetterWrite/hooks/useWriteLetter.ts
@@ -1,29 +1,30 @@
 import React, { useCallback, useState } from 'react';
 import { useStore } from 'zustand';
 import { useBottomToastStore } from '../../../commons/components/BottomToastList/stores/useBottomToastStore';
-import delay from '../../../commons/utils/delay';
+import useThrottleCallback from '../../../commons/hooks/useThrottleCallback';
 
 export const TEXT_LIMIT = 5;
 
 export default function useWriteLetter() {
   const addToast = useStore(useBottomToastStore, (state) => state.addToast);
   const [text, setText] = useState('');
-  const [isActive, setIsActive] = useState(true);
+  const throttleAddToast = useThrottleCallback(
+    () => {
+      addToast({ children: `텍스트는 ${TEXT_LIMIT}자 이하로 입력해주세요` });
+    },
+    2000,
+    [addToast]
+  );
 
   const handleOnChange = useCallback(
     async (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       if (e.currentTarget.value.length > TEXT_LIMIT) {
-        if (isActive) {
-          addToast({ children: `텍스트는 ${TEXT_LIMIT}자 이하로 입력해주세요` });
-          setIsActive(false);
-          await delay(2);
-          setIsActive(true);
-        }
+        throttleAddToast();
         return;
       }
       setText(e.currentTarget.value);
     },
-    [addToast, isActive]
+    [throttleAddToast]
   );
 
   const handleOnSave = useCallback(() => {
