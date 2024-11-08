fad1827ed3a10d0b7350af1dc1223d31c58bcd74
youngju6143 <youngju6143@naver.com>
2024-11-09 03:36:58 +0900

    ✨ Feat : footer icon 구현 및 활성화 여부에 따라 색상 설정

--- src/commons/components/Footer/index.tsx
+++ src/commons/components/Footer/index.tsx
@@ -8,6 +8,8 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./components/Button";
 import WriteButton from "./components/WriteButton";
+import HomeIcon from "./components/Button/HomeIcon";
+import FriendIcon from "./components/Button/FriendIcon";
 
 interface FooterProps {
   isHome?: boolean;
@@ -23,12 +25,14 @@ export default function Footer({ isHome, isFriend }: FooterProps) {
         isDisabled={isHome}
         text="홈"
         onClick={() => navigate('/')}
+        icon={<HomeIcon isHome={isHome}/>}
       />
-      <WriteButton onClick={() => navigate('/letter/write')} />
+      <WriteButton onClick={() => navigate('/letters/write')} />
       <Button
         isDisabled={isFriend}
         text="친구"
         onClick={() => navigate('/friend')}
+        icon={<FriendIcon isFriend={isFriend}/>}
       />
     </div>
   );
