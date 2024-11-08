72fb6ec709cc30473f734879a047c3f900f133fa
youngju6143 <youngju6143@naver.com>
2024-11-09 02:23:37 +0900

    ✨ Feat :  그래프 컴포넌트 추가

--- src/pages/Home/components/Graph/GraphBox.tsx
+++ src/pages/Home/components/Graph/GraphBox.tsx
@@ -1,11 +1,49 @@
-/*
- 페이지나 대시보드 내에서 그래프를 표시할 때 사용되는 컴포넌트
-*/
+import { useEffect, useState } from 'react';
+import DataFiled from '../../../../commons/components/DoughnutGraph/type';
+import DoughnutGraph from '../../../../commons/components/DoughnutGraph';
+import delay from '../../../../commons/utils/delay';
 
-const GraphBox = () => {
-  return (
-    <div>GraphBox</div>
-  )
+const data: DataFiled[] = [
+  {
+    value: 3,
+    color: 'red', // 'rgba(.., .., .., ..)' | '#FF0000'
+    label: '화남', // 범례에 표시될 문자열
+    Tooltip: <MyTooltip text="화남" />,
+    tooltipContainerClassName: 'bg-red-600',
+  },
+  {
+    value: 5,
+    color: 'rgba(0,0,255,1)', // 'rgba(.., .., .., ..)' | '#FF0000'
+    label: '슬픔', // 범례에 표시될 문자열
+    Tooltip: <MyTooltip text="슬픔" />,
+    tooltipContainerClassName: 'bg-blue-600',
+  },
+  {
+    value: 10,
+    color: '#00FF00', // 'rgba(.., .., .., ..)' | '#FF0000'
+    label: '편함', // 범례에 표시될 문자열
+    Tooltip: <MyTooltip text="편함" />,
+    tooltipContainerClassName: 'bg-green-600',
+  },
+];
+
+export default function GraphBox() {
+  return <DoughnutGraph className="w-11/12 margin" data={data} />;
 }
 
-export default GraphBox;
+function MyTooltip({ text }: { text: string }) {
+  const [loadText, setLoadText] = useState<string | undefined>(undefined);
+  useEffect(() => {
+    mockCall(text).then((res) => {
+      setLoadText(res);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <div className="w-20 h-20 bg-black opacity-60 text-white">{loadText ?? '로딩중...'}</div>;
+}
+
+const mockCall = async (text: string) => {
+  await delay(1);
+  return text;
+};
