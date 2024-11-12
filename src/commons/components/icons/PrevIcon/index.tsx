91d3aedd88cf595b1ee92a07a7d2eeb9abf7b635
youngju6143 <youngju6143@naver.com>
2024-11-12 19:52:38 +0900

    ✨ Feat :  prevIcon 추가

---
+++ src/commons/components/icons/PrevIcon/index.tsx
@@ -0,0 +1,13 @@
+export default function PrevIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8 20L16 12L8 4"
+        stroke="#1E1E20"
+        stroke-width="1.5"
+        stroke-linecap="square"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
