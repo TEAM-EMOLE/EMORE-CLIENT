066c1d72b5c1fcae814265ffe3316aa6ef9b28da
youngju6143 <youngju6143@naver.com>
2024-11-12 19:54:16 +0900

    💄 Design:  내가 쓴 편지함, 받은 편지함 버튼 디자인 반영

--- src/pages/Home/components/ReceiveLetter/index.tsx
+++ src/pages/Home/components/ReceiveLetter/index.tsx
@@ -2,11 +2,14 @@
   받은 편지함 페이지로 이동하는 버튼을 포함한 컴포넌트
 */
 
+import PrevIcon from '../../../../commons/components/icons/PrevIcon';
+
 export default function ReceiveLetter() {
+  let count = 10; // 편지 개수, 추후 API 연동 시 수정
   return (
-    <div className="px-3 w-11/12 h-[54px] flex items-center justify-between border-2 cursor-pointer">
-      <p>받은 편지함</p>
-      <button>)</button>
+    <div className="flex items-center justify-between px-3 w-11/12 h-[54px] bg-[#f9f9f9] border border-[#e8e8e8] rounded-[10px] text-[1rem] font-bold cursor-pointer">
+      <p>받은 편지함 {count}</p>
+      <PrevIcon />
     </div>
   );
 }
--- src/pages/Home/components/SendLetter/index.tsx
+++ src/pages/Home/components/SendLetter/index.tsx
@@ -2,11 +2,15 @@
   보낸 편지함 페이지로 이동하는 버튼을 포함한 컴포넌트
 */
 
+import PrevIcon from '../../../../commons/components/icons/PrevIcon';
+
 export default function SendLetter() {
+  let count = 10; // 편지 개수, 추후 API 연동 시 수정
+
   return (
-    <div className="flex items-center justify-between px-3 w-11/12 h-[54px] border-2 cursor-pointer">
-      <p>내가 쓴 편지함</p>
-      <button>)</button>
+    <div className="flex items-center justify-between px-3 w-11/12 h-[54px] bg-[#f9f9f9] border border-[#e8e8e8] rounded-[10px] text-[1rem] font-bold cursor-pointer">
+      <p>내가 쓴 편지함 {count}</p>
+      <PrevIcon />
     </div>
   );
 }
